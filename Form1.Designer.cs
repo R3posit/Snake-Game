2192f4e2d2df62007790b76e4dd40980c7dceed7
R3posit <keremkbdev@gmail.com>
2025-03-10 23:33:16 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.yilansBasi = new System.Windows.Forms.PictureBox();
+            this.yilanGovde = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.yilansBasi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yilanGovde)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // yilansBasi
+            // 
+            this.yilansBasi.BackColor = System.Drawing.Color.Transparent;
+            this.yilansBasi.Image = ((System.Drawing.Image)(resources.GetObject("yilansBasi.Image")));
+            this.yilansBasi.Location = new System.Drawing.Point(1317, 12);
+            this.yilansBasi.Name = "yilansBasi";
+            this.yilansBasi.Size = new System.Drawing.Size(40, 30);
+            this.yilansBasi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.yilansBasi.TabIndex = 0;
+            this.yilansBasi.TabStop = false;
+            // 
+            // yilanGovde
+            // 
+            this.yilanGovde.Image = ((System.Drawing.Image)(resources.GetObject("yilanGovde.Image")));
+            this.yilanGovde.Location = new System.Drawing.Point(1389, 12);
+            this.yilanGovde.Name = "yilanGovde";
+            this.yilanGovde.Size = new System.Drawing.Size(40, 30);
+            this.yilanGovde.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.yilanGovde.TabIndex = 0;
+            this.yilanGovde.TabStop = false;
+            this.yilanGovde.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1447, 1055);
+            this.Controls.Add(this.yilanGovde);
+            this.Controls.Add(this.yilansBasi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.yilansBasi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yilanGovde)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox yilansBasi;
+        private System.Windows.Forms.PictureBox yilanGovde;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int rotation = 0;
+        private int hiz = 5;
+        private PictureBox yem;
+        private Random rnd = new Random();
+        private bool oyunBitti = false;
+
+        List<PictureBox> yilanParcalari = new List<PictureBox>();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            yilansBasi.Location = new Point(200, 200);
+            yilansBasi.Width = 20;
+            yilansBasi.Height = 20;
+            yilanParcalari.Add(yilansBasi);
+            this.Controls.Add(yilansBasi);
+
+            YemOlustur();
+            
+            timer1.Start();
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.W)
+            {
+                // üst yön
+                if (rotation != 1)
+                {
+                    rotation = 0;
+                }
+
+            } else if(e.KeyCode == Keys.S)
+            {
+                // alt yön
+                if (rotation != 0)
+                {
+                    rotation = 1;
+                }
+
+            } else if(e.KeyCode == Keys.D)
+            {
+                // sağa dönüş
+                if (rotation != 3)
+                {
+                    rotation = 2;
+                }
+
+
+            } else if(e.KeyCode == Keys.A)
+            {
+                // sola dönüş !! Dikkat burada sağa gidiyorken aniden sola dönemez ÇAKIŞMA gerçekleşir
+                if (rotation != 2)
+                {
+                   rotation = 3;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            PictureBox yilanBasi = yilanParcalari[0];
+            Point yeniKonum = yilanBasi.Location;
+
+            if (rotation == 0) yeniKonum.Y -= hiz;
+            if (rotation == 1) yeniKonum.Y += hiz;
+            if (rotation == 2) yeniKonum.X += hiz;
+            if (rotation == 3) yeniKonum.X -= hiz;
+
+            yilanBasi.Location = yeniKonum;
+
+
+            for (int i = yilanParcalari.Count - 1; i > 0; i--)
+            {
+                yilanParcalari[i].Location = yilanParcalari[i - 1].Location;
+            }
+
+
+            if (yilanBasi.Location.X < 0 || yilanBasi.Location.X > this.Width - hiz ||
+               yilanBasi.Location.Y < 0 || yilanBasi.Location.Y > this.Height - hiz)
+            {
+                OyunBitir();
+                return;
+            }
+
+            // yem yeme
+            if (yilanBasi.Bounds.IntersectsWith(yem.Bounds))
+            {
+                YemYe();
+            }
+        }
+
+
+        // yem olusturam
+        private void YemOlustur()
+        {
+            yem = new PictureBox
+            {
+                Size = new Size(20, 20),
+                BackColor = Color.Red,
+                Location = new Point(rnd.Next(1, this.Width / hiz) * hiz, rnd.Next(1, this.Height / hiz) * hiz)
+            };
+
+            this.Controls.Add(yem);
+        }
+
+        // yemece
+        private void YemYe()
+        {
+            PictureBox sonParca = yilanParcalari[yilanParcalari.Count - 1];
+            Point yeniParcaKonum = sonParca.Location;
+
+            if (rotation == 0) yeniParcaKonum.Y += 40;
+            if (rotation == 1) yeniParcaKonum.Y -= 40; 
+            if (rotation == 2) yeniParcaKonum.X -= 40;
+            if (rotation == 3) yeniParcaKonum.X += 40;
+
+            PictureBox yeniParca = new PictureBox
+            {
+                Size = new Size(20, 20),
+                Image = yilanGovde.Image,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Location = yeniParcaKonum
+            };
+
+            yilanParcalari.Add(yeniParca);
+            this.Controls.Add(yeniParca);
+
+            this.Controls.Remove(yem);
+            YemOlustur();
+        }
+
+
+        // bye bye
+        private void OyunBitir()
+        {
+            oyunBitti = true;
+            timer1.Stop();
+            MessageBox.Show("Oyun Bitti!");
+        }
+
+
+    }
+}
